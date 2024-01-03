66764bdc7cf87b3bfe15fe8f02c51079b592a738
Chris Morton <cosmo@cosmo.2y.net>
2024-01-03 15:50:05 -0600

    updated to use spark/navx

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,9 +29,11 @@ import frc.robot.commands.DriveCommands;
 import frc.robot.commands.FeedForwardCharacterization;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIONavX2;
 import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOSparkMax;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
 import frc.robot.subsystems.vision.AprilTagVision;
 import frc.robot.subsystems.vision.AprilTagVisionIO;
@@ -64,21 +66,22 @@ public class RobotContainer {
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
-        // drive =
-        //     new Drive(
-        //         new GyroIOPigeon2(false),
-        //         new ModuleIOSparkMax(0),
-        //         new ModuleIOSparkMax(1),
-        //         new ModuleIOSparkMax(2),
-        //         new ModuleIOSparkMax(3));
-        // flywheel = new Flywheel(new FlywheelIOSparkMax());
-        drive =
-            new Drive(
-                new GyroIOPigeon2(true),
-                new ModuleIOTalonFX(0),
-                new ModuleIOTalonFX(1),
-                new ModuleIOTalonFX(2),
-                new ModuleIOTalonFX(3));
+         drive =
+             new Drive(
+                 new GyroIONavX2(),
+                 new ModuleIOSparkMax(0),
+                 new ModuleIOSparkMax(1),
+                 new ModuleIOSparkMax(2),
+                 new ModuleIOSparkMax(3));
+         //flywheel = new Flywheel(new FlywheelIOSparkMax());
+        //drive =
+        //    new Drive(
+        //        new GyroIOPigeon2(true),
+        //        new ModuleIOTalonFX(0),
+        //        new ModuleIOTalonFX(1),
+        //        new ModuleIOTalonFX(2),
+        //        new ModuleIOTalonFX(3));
+        
         aprilTagVision = new AprilTagVision(new AprilTagVisionIOLimelight("limelight"));
 
         // flywheel = new Flywheel(new FlywheelIOTalonFX());
