d1389526cdeb1dd941b41921185d4b180d14cb19
Jian <jiansundvall@gmail.com>
2024-01-13 16:26:19 -0600

    add support for talonfx

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -2,8 +2,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeConstants;
-import frc.robot.subsystems.intake.IntakeSubsystem;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
@@ -11,23 +11,21 @@ public class IntakeCommands {
 
   private IntakeCommands() {}
 
-  public static Command triggerIntake(
-      IntakeSubsystem intakeSubsystem, DoubleSupplier speedSupplier) {
+  public static Command triggerIntake(Intake intakeSubsystem, DoubleSupplier speedSupplier) {
     return Commands.run(
         () ->
-            intakeSubsystem.setVelocity(
+            intakeSubsystem.runVelocity(
                 IntakeConstants.MAX_RAD_PER_SEC * speedSupplier.getAsDouble()),
         intakeSubsystem);
   }
 
-  public static Command buttonIntake(
-      IntakeSubsystem intakeSubsystem, BooleanSupplier isIntakeNote) {
+  public static Command buttonIntake(Intake intakeSubsystem, BooleanSupplier isIntakeNote) {
     return Commands.run(
         () -> {
           if (isIntakeNote.getAsBoolean()) {
-            intakeSubsystem.setVelocity(IntakeConstants.AUTO_RAD_PER_SEC);
+            intakeSubsystem.runVelocity(IntakeConstants.AUTO_RAD_PER_SEC);
           } else {
-            intakeSubsystem.setVelocity(-IntakeConstants.AUTO_RAD_PER_SEC);
+            intakeSubsystem.runVelocity(-IntakeConstants.AUTO_RAD_PER_SEC);
           }
         });
   }
