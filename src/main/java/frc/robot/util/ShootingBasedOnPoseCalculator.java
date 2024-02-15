7f3697499297fd92a22c5e16ad211b7eee5cc739
Jian <jiansundvall@gmail.com>
2024-02-15 11:32:33 -0600

    add comments

--- src/main/java/frc/robot/util/ShootingBasedOnPoseCalculator.java
+++ src/main/java/frc/robot/util/ShootingBasedOnPoseCalculator.java
@@ -7,6 +7,8 @@ public class ShootingBasedOnPoseCalculator {
 
   private ShootingBasedOnPoseCalculator() {}
 
+  //This method assumes that the note's velocity is high enough that its trajectory can
+  //be approximated by a straight line. It aims at the top of the speaker
   public static double calculateAngleInRadiansWithConstantVelocity(Pose2d robotPose) {
     Pose2d speakerPose = AllianceFlipUtil.apply(
             FieldConstants.Speaker.centerSpeakerOpening);
