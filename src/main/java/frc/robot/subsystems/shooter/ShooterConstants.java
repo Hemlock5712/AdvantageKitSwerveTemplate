88cfd7917f6c537e4d5a85657acc624651c68db9
kyle <75395106+ehre9305@users.noreply.github.com>
2024-02-19 20:57:27 -0600

    mostly working shooter velocity control, bottom shooter needs tuning

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -5,7 +5,7 @@ public final class ShooterConstants {
   public static final int BOTTOM_MOTOR_ID = 15;
   public static final double RUN_VOLTS = 6;
   public static final double AMP_VELOCITY_RAD_PER_SEC = 2 * Math.PI;
-  public static final double SPEAKER_VELOCITY_RAD_PER_SEC = 0.0;
+  public static final double SPEAKER_VELOCITY_RAD_PER_SEC = 280;
   public static final double CLOSED_LOOP_RAMP_RATE = 0.01;
   public static final double OPEN_LOOP_RAMP_RATE = 0.01;
   public static final double VELOCITY_TOLERANCE = 0.08;
@@ -19,25 +19,25 @@ public final class ShooterConstants {
   public static final class Real {
     public static final class FeedForwardConstants {
       public static final class TopConstants {
-        public static final double kS = 0.2;
-        public static final double kV = 0.05;
+        public static final double kS = 0.097527;
+        public static final double kV = 0.02056;
       }
 
       public static final class BottomConstants {
-        public static final double kS = 0.2;
-        public static final double kV = 0.05;
+        public static final double kS = 0.055714;
+        public static final double kV = 0.021749;
       }
     }
 
     public static final class PIDConstants {
       public static final class TopConstants {
-        public static final double kP = 0.1;
+        public static final double kP = 0.00011219;
         public static final double kI = 0.0;
         public static final double kD = 0.0;
       }
 
       public static final class BottomConstants {
-        public static final double kP = 0.1;
+        public static final double kP = 0.0059535;
         public static final double kI = 0.0;
         public static final double kD = 0.0;
       }
